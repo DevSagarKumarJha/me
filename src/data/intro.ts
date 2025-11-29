a58d17799e9da825195b1a1e45cb937afb5b1d0c
Your Name <devsagarkumarjha@gmail.com>
2025-11-29 18:53:41 +0530

    --created: intro.ts

---
+++ src/data/intro.ts
@@ -0,0 +1,12 @@
+export const Intro = `{
+  "name": "Sagar Kumar Jha",
+  "location": "Delhi",
+  "email": "devsagarkumarjha@gmail.com",
+  "open_for_work": true,
+  "bio": "Full-stack dev (MERN, Prisma)",
+  "twitter_username": "DevSagarKrJha",
+  "linked_in_username": "devsagarkumarjha",
+  "github_username": "devsagarkumarjha",
+  "peerlist_username": "sagarkumarjha",
+  "leetcode_username": "devsagarkumarjha"
+}`;
